224bb431fb0bc40e4c5a5c0d32e5789f740cfd58
Racekid16 <1750801177a@gmail.com>
2026-01-09 13:55:09 -0500

    Make buttons wider

--- src/components/buttons/verbalDuelButtons.ts
+++ src/components/buttons/verbalDuelButtons.ts
@@ -1,19 +1,21 @@
 import { ButtonStyles, type MessageComponent } from "oceanic.js";
 
 export function verbalDuelButtons(challengerId: string, opponentId: string): MessageComponent {
+    const padding = "\u200B \u200B \u200B \u200B \u200B \u200B \u200B \u200B";
+
     return {
         type: 1,    // ACTION_ROW
         components: [
             {
                 type: 2,    // BUTTON
                 customID: `verbalDuel:accept:${challengerId}:${opponentId}`,
-                label: "Accept",
+                label: `${padding} Accept ${padding}`,
                 style: ButtonStyles.SUCCESS,
             },
             {
                 type: 2,    // BUTTON
                 customID: `verbalDuel:decline:${challengerId}:${opponentId}`,
-                label: "Decline",
+                label: `${padding} Decline ${padding}`,
                 style: ButtonStyles.DANGER,
             },
         ],
--- src/components/buttons/verbalTestButtons.ts
+++ src/components/buttons/verbalTestButtons.ts
@@ -1,19 +1,21 @@
 import { ButtonStyles, type MessageComponent } from "oceanic.js";
 
 export function verbalTestButtons(userId: string): MessageComponent {
+    const padding = "\u200B \u200B \u200B \u200B \u200B \u200B \u200B \u200B";
+    
     return {
         type: 1,    // ACTION_ROW
         components: [
             {
                 type: 2,    // BUTTON
                 customID: `verbalTest:seen:${userId}`,
-                label: "Seen",
+                label: `${padding} Seen ${padding}`,
                 style: ButtonStyles.PRIMARY,
             },
             {
                 type: 2,    // BUTTON
                 customID: `verbalTest:new:${userId}`,
-                label: "New",
+                label: `${padding} New ${padding}`,
                 style: ButtonStyles.SUCCESS,
             },
         ],
